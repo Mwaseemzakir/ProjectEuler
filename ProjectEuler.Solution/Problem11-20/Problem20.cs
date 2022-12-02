e158314b15174b1015d8517b15fc96916b3cbe2c
Muhammad Waseem <mwaseem@cinnova.com>
2022-12-02 20:27:20 +0500

    Problem 20 Solution

---
+++ ProjectEuler.Solution/Problem11-20/Problem20.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Solution.Problem11_20
+{
+    public static class Problem20
+    {
+        public static void Solution()
+        {
+            int input = 100;
+            BigInteger factorial = Factorial(input);
+            int sum = factorial.ToString().Select(x => Convert.ToInt32(x.ToString())).ToList().Sum();
+            Console.WriteLine("Sum of factorial " + " is  : " + sum);
+
+        }
+
+        private static BigInteger Factorial(int input)
+        {
+            if (input == 1) return 1;
+            else return input * Factorial(input - 1);
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -1,15 +1,18 @@
 ﻿using ProjectEuler.Core.Data;
 using ProjectEuler.Core.IO;
 using ProjectEuler.Core.Utilities;
+using ProjectEuler.Solution.Problem11_20;
 
 Console.WriteLine("Which problem's solution you would like to see !");
 int problemNo = 0;
 bool isValidNumber = Int32.TryParse(Console.ReadLine(), out problemNo);
 if(isValidNumber)
 {
+    Console.WriteLine(" / / / / / / / / / / / / / / / / / /  P r o b l e m / / / / / / / / / / / / / / / / / /");
     string html = await UrlReader.ReadUrl("https://projecteuler.net/minimal=1");
     string text = HtmlConverter.HtmlToText(html);
     Console.WriteLine(text);
+    Problem20.Solution();
 }
 else
 {
